4d2e56316b2ee7fb5fc9640f5dc85d81afb49b32
Burin Choomnuan <burin.choomnuan@gmail.com>
2021-05-15 13:54:43 -0400

    Fix some warning

--- src/net/b12n/github/utils/core.clj
+++ src/net/b12n/github/utils/core.clj
@@ -6,7 +6,7 @@
             [net.b12n.github.utils.option :as opt]
             [me.raynes.fs :as fs]
             [tentacles.repos :as repos]
-            [clj-jgit.porcelain :as cjp :refer :all]
+            [clj-jgit.porcelain :as cjp]
             [clojure.java.shell :as shell]))
 
 (defn git-init-commit
@@ -54,8 +54,9 @@
       fs/expand-home
       fs/normalized))
 
-(defn- default-options [options]
+(defn- default-options
   "Define the sensible default options when creating new Github repository"
+  [options]
   (merge {:public false
           :has-issue false
           :has-wiki false
@@ -75,35 +76,35 @@
   (let [config-options (:config options)
         reponame (:repo options)]
     (try
-      (if-let [config (read-config (expand-and-normalize config-options))]
+      (when-let [config (read-config (expand-and-normalize config-options))]
         (let [username (:username config)
               password (:password config)
-              auth (str username ":" password)]
-          (let [github-prefix "https://github.com/"
-                homepage (str github-prefix (str/join "/" (list username reponame)))
-                response (repos/create-repo reponame (default-options {:auth auth
-                                                                       :description (str reponame " by " username)
-                                                                       :homepage homepage}))]
-            (check-and-confirm-response response)
+              auth (str username ":" password)
+              github-prefix "https://github.com/"
+              homepage (str github-prefix (str/join "/" (list username reponame)))
+              response (repos/create-repo reponame (default-options {:auth auth
+                                                                     :description (str reponame " by " username)
+                                                                     :homepage homepage}))]
+          (check-and-confirm-response response)
 
-            ;; Make sure that we are running from the right directory
-            (let [base-dir (fs/file ".")
-                  {:keys [init-commit
-                          remote-label
-                          push]} options]
+          ;; Make sure that we are running from the right directory
+          (let [base-dir (fs/file ".")
+                {:keys [init-commit
+                        remote-label
+                        push]} options]
 
-              ;; Don't run git-init commit if project already containing .git directory
-              (if-not (fs/exists? ".git")
-                (if init-commit (git-init-commit base-dir)))
+            ;; Don't run git-init commit if project already containing .git directory
+            (when-not (fs/exists? ".git")
+              (when init-commit (git-init-commit base-dir)))
 
-              ;; We always add remote tracking
-              (git-add-remote username reponame base-dir remote-label)
+            ;; We always add remote tracking
+            (git-add-remote username reponame base-dir remote-label)
 
-              ;; Run git push only if the user specify '--push' option.
-              (if (fs/exists? ".git")
-                (if push (git-push-remote base-dir remote-label ""))
-                ;; No valid git project, so let the the user know
-                (println "FYI: As you don't have the valid git project, you may like to run git init && git push manually."))))))
+            ;; Run git push only if the user specify '--push' option.
+            (if (fs/exists? ".git")
+              (when push (git-push-remote base-dir remote-label ""))
+              ;; No valid git project, so let the the user know
+              (println "FYI: As you don't have the valid git project, you may like to run git init && git push manually.")))))
       (opt/exit 0 "Done")
       (catch Exception e
         ;; Handle any problem/exception that we may have
